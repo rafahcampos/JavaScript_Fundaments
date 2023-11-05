468c743556dbc87e0251ae5b4c9541a85e9a9ea2
Rafael Campos <rafael.campos.m@hotmail.com>
2023-11-05 13:29:01 -0300

    Javascript Fundaments

---
+++ type-number.js
@@ -0,0 +1,16 @@
+const meuNumero = 3;
+
+const primeiro = 2;
+const segundo = 5;
+
+const soma = primeiro * segundo;
+
+console.log(soma);
+
+//flutuante
+
+const float = 3.5;
+
+const float2 = float * segundo * primeiro;
+
+console.log(float2)
